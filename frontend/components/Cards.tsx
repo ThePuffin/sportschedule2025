06c09cb3d270d1d4f71a10977b322ada682892ce
ThePuffin <ballutvincent@gmail.com>
2025-01-12 13:33:56 +0100

    create card of the game

---
+++ frontend/components/Cards.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+import { Card } from '@rneui/base';
+import { View, Image, Text } from 'react-native';
+
+export default function Cards({ data }) {
+  const { homeTeam, awayTeam, arenaName, timeStart } = data;
+  console.log(homeTeam, awayTeam, 'ici');
+  return (
+    <ThemedView>
+      <Card containerStyle={{}} wrapperStyle={{}}>
+        <Card.Title>
+          {timeStart} @ {arenaName}
+        </Card.Title>
+        <Card.Divider />
+        <View
+          style={{
+            position: 'relative',
+            alignItems: 'center',
+          }}
+        >
+          <Text>{homeTeam}</Text>
+          <Image
+            style={{ width: '50%', height: 50 }}
+            resizeMode="contain"
+            source={{
+              uri: 'https://avatars0.githubusercontent.com/u/32242596?s=460&u=1ea285743fc4b083f95d6ee0be2e7bb8dcfc676e&v=4',
+            }}
+          />
+          <Text>vs</Text>
+          <Image
+            style={{ width: '50%', height: 50 }}
+            resizeMode="contain"
+            source={{
+              uri: 'https://avatars0.githubusercontent.com/u/32242596?s=460&u=1ea285743fc4b083f95d6ee0be2e7bb8dcfc676e&v=4',
+            }}
+          />
+          <Text>{awayTeam}</Text>
+        </View>
+      </Card>
+    </ThemedView>
+  );
+}
