ca1a4e7e74fcc64baa19c51eab737a10d2bef880
ThePuffin <ballutvincent@gmail.com>
2025-02-01 18:28:05 +0100

    add multiple accordions

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
-import { Collapsible } from '@/components/Collapsible';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import React, { useEffect, useState } from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
-
+import { League } from '../../constants/enum.ts';
 import Cards from '../../components/Cards';
+import { ListItem } from '@rneui/themed';
 
 interface GameFormatted {
   _id: string;
@@ -44,13 +44,54 @@ const getGamesFromApi = async (): Promise<GameFormatted[]> => {
 };
 
 export default function GameofTheDay() {
+  const [expanded, setExpanded] = useState({});
   const [games, setGames] = useState<GameFormatted[]>([]);
-  const makeCards = () => {
-    if (!games) {
+  const leagues = Object.keys(League)
+    .filter((item) => {
+      return isNaN(Number(item));
+    })
+    .sort();
+  if (Object.entries(expanded).length === 0) {
+    leagues.forEach((league, i) => {
+      expanded[league] = i === 0;
+    });
+    setExpanded(expanded);
+  }
+
+  // TODO: create component accordion
+  const displayContent = () => {
+    return leagues.map((league, i) => {
+      return (
+        <ListItem.Accordion
+          content={
+            <>
+              <ListItem.Content>
+                <ListItem.Title>{league}</ListItem.Title>
+              </ListItem.Content>
+            </>
+          }
+          isExpanded={expanded[league]}
+          onPress={() => {
+            expanded[league] = !expanded[league];
+            // setExpanded(!expanded);
+          }}
+        >
+          {makeCards(league)}
+        </ListItem.Accordion>
+      );
+    });
+  };
+  const makeCards = (league) => {
+    let gamesToShow = [...games];
+    if (league !== League.ALL) {
+      gamesToShow = gamesToShow.filter((game) => game.league === league);
+    }
+
+    if (!gamesToShow || !gamesToShow.length) {
       return <ThemedText>There are no games today</ThemedText>;
     }
-    if (games.length) {
-      return games.map((game) => {
+    if (gamesToShow.length) {
+      return gamesToShow.map((game) => {
         const gameId = game?._id || Math.random();
         return <Cards key={gameId} data={game} />;
       });
@@ -69,12 +110,7 @@ export default function GameofTheDay() {
   return (
     <>
       <ScrollView>
-        <ThemedView>
-          <Collapsible title="click me">
-            <ThemedText>click</ThemedText>
-          </Collapsible>
-          {makeCards()}
-        </ThemedView>
+        <ThemedView>{displayContent()}</ThemedView>
       </ScrollView>
     </>
   );
---
+++ frontend/constants/enum.ts
@@ -0,0 +1,7 @@
+export enum League {
+  ALL = 'ALL',
+  MLB = 'MLB',
+  NBA = 'NBA',
+  NHL = 'NHL',
+  NFL = 'NFL',
+}
