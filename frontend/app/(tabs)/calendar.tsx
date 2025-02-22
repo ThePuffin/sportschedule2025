1d8183883a81d6e08e6654fa31ce5b6d26d465f2
ThePuffin <ballutvincent@gmail.com>
2025-02-22 14:32:49 +0100

    define default team to start

--- frontend/app/(tabs)/calendar.tsx
+++ frontend/app/(tabs)/calendar.tsx
@@ -1,58 +1,78 @@
-import { ThemedView } from '@/components/ThemedView'
-import { Button } from '@rneui/themed'
-import React, { useEffect, useState } from 'react'
-import { ScrollView } from 'react-native'
-import Buttons from '../../components/Buttons'
-import Cards from '../../components/Cards'
-import Selector from '../../components/Selector'
-import { readableDate } from '../../utils/date'
-import { FilterGames, GameFormatted, Team } from '../../utils/types'
+import { ThemedView } from '@/components/ThemedView';
+import { Button } from '@rneui/themed';
+import React, { useEffect, useState } from 'react';
+import { ScrollView } from 'react-native';
+import Buttons from '../../components/Buttons';
+import Cards from '../../components/Cards';
+import Selector from '../../components/Selector';
+import { readableDate } from '../../utils/date';
+import { FilterGames, GameFormatted, Team } from '../../utils/types';
+import { randomNumber } from '../../utils/utils';
 
 export default function Calendar() {
-  const [games, setGames] = useState<FilterGames>({})
-  const [teams, setTeams] = useState<Team[]>([])
-  const [teamsSelected, setTeamsSelected] = useState<string[]>([])
+  const [games, setGames] = useState<FilterGames>({});
+  const [teams, setTeams] = useState<Team[]>([]);
+  const [teamsSelected, setTeamsSelected] = useState<string[]>([]);
+
+  const getSelectedTeams = (allTeams) => {
+    const selection = [];
+    //TODO: get datas from storage
+    if (!selection.length) {
+      while (selection.length < 2) {
+        const randomId = allTeams[randomNumber(allTeams.length) - 1]?.uniqueId;
+
+        if (randomId && !selection.includes(randomId)) {
+          selection.push(randomId);
+        }
+      }
+    }
+    setTeamsSelected(selection);
+  };
 
   const getTeamsFromApi = async (): Promise<Team[]> => {
     try {
-      const response = await fetch(`http://localhost:3000/teams`)
-      return await response.json()
+      const response = await fetch(`http://localhost:3000/teams`);
+      const allTeams = await response.json();
+      getSelectedTeams(allTeams);
+      return allTeams;
     } catch (error) {
-      console.error(error)
-      return []
+      console.error(error);
+      return [];
     }
-  }
+  };
 
   const getGamesFromApi = async (): Promise<FilterGames> => {
     if (teamsSelected && teamsSelected.length !== 0) {
-      const now = new Date()
-      const startDate = readableDate(now)
-      const endDate = readableDate(new Date(now.setMonth(now.getMonth() + 1)))
+      const now = new Date();
+      const startDate = readableDate(now);
+      const endDate = readableDate(new Date(now.setMonth(now.getMonth() + 1)));
 
       try {
         const response = await fetch(
-          `http://localhost:3000/games/filter?startDate=${startDate}&endDate=${endDate}&teamSelectedIds=${teamsSelected.join(',')}`
-        )
-        return await response.json()
+          `http://localhost:3000/games/filter?startDate=${startDate}&endDate=${endDate}&teamSelectedIds=${teamsSelected.join(
+            ','
+          )}`
+        );
+        return await response.json();
       } catch (error) {
-        console.error(error)
-        return {}
+        console.error(error);
+        return {};
       }
     }
-    return {}
-  }
+    return {};
+  };
 
   const handleTeamSelectionChange = (teamSelectedId: string, i: number) => {
     setTeamsSelected((prevTeamsSelected) => {
-      const newTeamsSelected = [...prevTeamsSelected]
-      newTeamsSelected[i] = teamSelectedId
-      return newTeamsSelected
-    })
-  }
+      const newTeamsSelected = [...prevTeamsSelected];
+      newTeamsSelected[i] = teamSelectedId;
+      return newTeamsSelected;
+    });
+  };
 
   const displayTeamSelector = () => {
     return teamsSelected.map((teamSelectedId, i) => {
-      const data = { i, activeTeams: teams, teamsSelectedIds: teamsSelected, teamSelectedId }
+      const data = { i, activeTeams: teams, teamsSelectedIds: teamsSelected, teamSelectedId };
       return (
         <td key={teamSelectedId}>
           <ThemedView>
@@ -60,51 +80,50 @@ export default function Calendar() {
             {displayGamesCards(teamSelectedId)}
           </ThemedView>
         </td>
-      )
-    })
-  }
+      );
+    });
+  };
 
   const displayGamesCards = (teamSelectedId: string) => {
     if (games) {
-      const days = Object.keys(games) || []
+      const days = Object.keys(games) || [];
       if (days.length) {
         return days.map((day: string) => {
-          const game = games[day].find((game: GameFormatted) => game.teamSelectedId === teamSelectedId)
+          const game = games[day].find((game: GameFormatted) => game.teamSelectedId === teamSelectedId);
           if (game) {
-            const gameId = game?._id || Math.random()
+            const gameId = game?._id ?? randomNumber(999999);
             return (
               <td key={gameId}>
                 <ThemedView>
                   <Cards data={game} showDate={true} />
                 </ThemedView>
               </td>
-            )
+            );
           }
-          return null
-        })
+          return null;
+        });
       }
     }
-    return <Button title="Solid" disabled={true} type="solid" loading />
-  }
+    return <Button title="Solid" disabled={true} type="solid" loading />;
+  };
 
   useEffect(() => {
     async function fetchTeams() {
-      const teamsData = await getTeamsFromApi()
-      setTeams(teamsData)
-      setTeamsSelected([teamsData[0]?.uniqueId, teamsData[1]?.uniqueId, teamsData[2]?.uniqueId])
+      const teamsData = await getTeamsFromApi();
+      setTeams(teamsData);
     }
-    fetchTeams()
-  }, [])
+    fetchTeams();
+  }, []);
 
   useEffect(() => {
     if (teamsSelected.length > 0) {
       async function fetchGames() {
-        const gamesData = await getGamesFromApi()
-        setGames(gamesData)
+        const gamesData = await getGamesFromApi();
+        setGames(gamesData);
       }
-      fetchGames()
+      fetchGames();
     }
-  }, [teamsSelected])
+  }, [teamsSelected]);
 
   return (
     <ScrollView>
@@ -115,5 +134,5 @@ export default function Calendar() {
         </tbody>
       </table>
     </ScrollView>
-  )
+  );
 }
