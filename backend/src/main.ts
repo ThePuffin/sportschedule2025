1c002764a7afadbfd28075bce0629aa9bd66b97f
ThePuffin <ballutvincent@gmail.com>
2025-03-15 11:10:39 +0100

    add dot env to start local mode

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 const PORT = parseInt(process.env.PORT, 10) || 3000;
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
